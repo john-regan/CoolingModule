3c37b8c319da961bfc0b8f6ddeeda05d8b769604
John Regan <john.a.regan@durham.ac.uk>
2015-11-23 15:53:04 +0000

    Added file hdf5_wrapper.h for function prototypes

---
+++ hdf5_wrapper.h
@@ -0,0 +1,10 @@
+/*
+ * Header file containing HDF5 wrapper function prototypes 
+ */
+
+
+/*
+ * Short description of what this function does (TODO)
+ */
+
+herr_t dataset_read(char *dataset_name,  int hdf5_type,  hid_t hdf5_file_handle, void *buf);
