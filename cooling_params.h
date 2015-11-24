a98b7373237ed79808220fd4bc34141fbb0402d7
Stefan Arridge <dc-arri1@cosma-b.cosma>
2015-11-24 10:30:49 +0000

    Updated to my current version

--- cooling_params.h
+++ cooling_params.h
@@ -7,30 +7,20 @@
 #define COOL_TABLE_TEMP_BINS      176
 #define COOL_TABLE_HE_BINS          7
 
-enum my_hdf5_types{
-  my_hdf5_int,
-  my_hdf5_long,
-  my_hdf5_float,
-  my_hdf5_double
-};
-enum my_hdf5_types hdf5_type;
-
 int n_h_bins_N, temp_bins_N, he_frac_bins_N;
-float *n_h_bins, *temp_bins, *he_frac_bins;
+float n_h_bins[COOL_TABLE_N_H_BINS], temp_bins[COOL_TABLE_TEMP_BINS], he_frac_bins[COOL_TABLE_HE_BINS];
+float net_cooling_rate[COOL_TABLE_HE_BINS][COOL_TABLE_TEMP_BINS][COOL_TABLE_N_H_BINS];
 
-
-
-herr_t attribute_read(char *attribute_name, enum my_hdf5_types hdf5_type,
-	       hid_t hdf5_file_handle, void *buf);
-
-extern struct cool_table_struct
+/*extern struct cool_table_struct
 {
   char filename[STR_LENGTH];
-  /*  int n_h_bins_N, temp_bins_N, he_frac_bins_N; */
+  int n_h_bins_N, temp_bins_N, he_frac_bins_N;
   float  n_h_bins[COOL_TABLE_N_H_BINS];
   float temp_bins[COOL_TABLE_TEMP_BINS];
   float  he_frac_bins[COOL_TABLE_HE_BINS] ;
   float net_cooling_rate[COOL_TABLE_N_H_BINS][COOL_TABLE_TEMP_BINS][COOL_TABLE_HE_BINS];
 } cool_table;
+
+*/
 
 
